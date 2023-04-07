b8a83cbbf365f9ec2904b03ee946ff5204fa4090
aliouafa1 <92216202+aliouafa1@users.noreply.github.com>
2023-04-07 09:58:48 +0200

    Update neuralnetworksheston.py

--- neuralnetworksheston.py
+++ neuralnetworksheston.py
@@ -1,1 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from scipy.stats import norm
+from scipy.integrate import quad
 
+# Define Heston model parameters
+v0 = 0.04  # initial variance
+theta = 0.04  # long-term variance
+kappa = 1.5  # rate of reversion
+sigma = 0.3  # volatility of variance
+rho = -0.6  # correlation
+
+# Define option parameters
+K = 100  # strike price
+T = 1  # time to maturity
+
+# Define neural network architecture
+n_hidden = 10
+n_neurons = 50
+learning_rate = 0.01
+n_epochs = 10000
+
+# Define option pricing function
+def heston_varswap_price(K, T):
+    # Define integrand function for calculating expected payoff
+    def integrand(u):
+        w = np.log(K) - 1j * u
+        d = np.sqrt((rho * sigma * 1j * u - kappa)**2 + sigma**2 * (1j * u + u**2))
+        g = (kappa - rho * sigma * 1j * u - d) / (kappa - rho * sigma * 1j * u + d)
+        C = v0 * (kappa - rho * sigma * 1j * u - d) / sigma**2 * (1 - np.exp(-d * T)) / (1 - g * np.exp(-d * T))
+        D = (kappa - rho * sigma * 1j * u - d) / sigma**2 * ((1 - np.exp(-d * T)) / (1 - g * np.exp(-d * T))) \
+            * (v0 / (2 * d) * (kappa - rho * sigma * 1j * u - d) * (1 - np.exp(-d * T)) + theta * (kappa - rho * sigma * 1j * u - d) * T \
+            + sigma**2 / (4 * d) * ((kappa - rho * sigma * 1j * u - d) * (1 - np.exp(-d * T)) - 2 * np.log((1 - g * np.exp(-d * T)) / (1 - g))))
+        return np.real(np.exp(-1j * u * np.log(K)) * np.exp(w * T) / (1j * u) * (C - D))
+
+    # Calculate expected payoff using numerical integration
+    expected_payoff, _ = quad(integrand, 0, np.inf, limit=100)
+    return (1 - np.exp(-theta * T)) * expected_payoff / np.pi
+
+# Generate training data (to be completed)
+n_samples = 1000
+K_vals = np.random.uniform(low=50, high=150, size=n_samples)
+T_vals = np.random.uniform(low=0.1, high=1, size=n_samples)
+X_train = np.column_stack((K_vals, T_vals))
+y_train = np.array([heston_varswap_price(K, T) for K, T in zip(K_vals, T_vals)])
+
+# Define neural network model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(n_neurons, activation="relu", input_shape=(2,)),
+    *[tf.keras.layers.Dense(n_neurons, activation="relu") for _ in range(n_hidden)],
+    tf.keras.layers.Dense(1)
+])
+
+
+# VarSwap specifications
+var_strike = np.array([0.04, 0.05, 0.06, 0.07, 0.08])
+var_swap_dates = np.array([0.5, 1.0, 1.5, 2.0])
+var_swap_vols = np.array([
+    [0.25, 0.22, 0.20, 0.18],
+    [0.24, 0.21, 0.19, 0.17],
+    [0.23, 0.20, 0.18, 0.16],
+    [0.22, 0.19, 0.17, 0.15],
+    [0.21, 0.18, 0.16, 0.14]
+])
+
+# VolSwap specifications
+vol_strike = np.array([0.04, 0.05, 0.06, 0.07, 0.08])
+vol_swap_dates = np.array([0.5, 1.0, 1.5, 2.0])
+vol_swap_vols = np.array([
+    [0.25, 0.24, 0.23, 0.22],
+    [0.22, 0.21, 0.20, 0.19],
+    [0.20, 0.19, 0.18, 0.17],
+    [0.18, 0.17, 0.16, 0.15],
+    [0.16, 0.15, 0.14, 0.13]
+])
