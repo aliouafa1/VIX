2d13b0cf1035d3ed2a3f900d60d850a2d0423cd3
aliouafa1 <92216202+aliouafa1@users.noreply.github.com>
2023-04-07 06:35:02 +0200

    Update sabrwithoutfinancepy.py

--- sabrwithoutfinancepy.py
+++ sabrwithoutfinancepy.py
@@ -1,13 +1,13 @@
 import numpy as np
 from scipy.stats import norm
 
-# Set up market data
+# we Set up market data
 spot = 100.0
 risk_free_rate = 0.01
 dividend_yield = 0.0
 atm_volatility = 0.20
 
-# Set up VarSwap parameters
+# we Set up VarSwap parameters (for the fixed rate it would be good to change it)
 varswap_start_date = "03-Apr-2022"
 varswap_end_date = "03-Apr-2023"
 varswap_fixed_rate = 0.02
@@ -17,17 +17,17 @@ volswap_start_date = "03-Apr-2022"
 volswap_end_date = "03-Apr-2023"
 volswap_fixed_rate = 0.03
 
-# Set up SABR model parameters
+# input SABR model parameters
 beta = 0.7
 rho = -0.3
 nu = 0.5
 volvol = 0.5
 
-# Set up volatility surface parameters
+# Let's set up volatility surface parameters
 strikes = np.arange(50, 151, 5)
 expiries = np.arange(0.25, 2.51, 0.25)
 
-# Define the SABR model function
+# Now we Define the SABR model function
 def sabr_volatility(strike, expiry, spot, beta, rho, nu, volvol):
     forward = spot * np.exp((risk_free_rate - dividend_yield) * expiry)
     alpha = forward ** (1 - beta)
@@ -38,10 +38,10 @@ def sabr_volatility(strike, expiry, spot, beta, rho, nu, volvol):
     factor3 = nu / alpha * x
     return factor1 * factor2 * factor3
 
-# Calcul of the fair values and comparison with ATMI
+# fair values and comparison with ATMI
 print("*** SABR Model Results ***")
 
-# Calculate VarSwap fair value
+# We Calculate VarSwap fair value
 varswap_fair_value = 0.0
 for i in range(len(strikes)):
     strike = strikes[i]
@@ -52,7 +52,7 @@ for i in range(len(strikes)):
 varswap_fair_value *= np.exp(-risk_free_rate * (varswap_end_date - varswap_start_date) / 365)
 
 
-# Calculate VolSwap fair value
+# We Calculate VolSwap fair value
 volswap_fair_value = 0.0
 for i in range(len(strikes)):
 strike = strikes[i]
@@ -62,7 +62,7 @@ d2 = d1 - vol * np.sqrt((volswap_end_date - volswap_start_date) / 365)
 volswap_fair_value += 2 * np.sqrt(volswap_start_date * volswap_end_date) * (spot * np.exp((risk_free_rate - dividend_yield) * (volswap_end_date - volswap_start_date) / 365) * norm.cdf(d1) - strike * np.exp(-risk_free_rate * (volswap_end_date - volswap_start_date) / 365) * norm.cdf(d2)) * norm.pdf(d1) * vol / np.sqrt((volswap_end_date - volswap_start_date) / 365)
 volswap_fair_value *= np.exp(-risk_free_rate * (volswap_end_date - volswap_start_date))
 
-# Comparison with ATMI
+# Print the Comparison with ATMI
 print("ATMI: ", atm_volatility)
 print("VarSwap Fair Value: ", varswap_fair_value)
 print("VolSwap Fair Value: ", volswap_fair_value)
